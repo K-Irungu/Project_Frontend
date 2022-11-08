08f59347cc1311bde059cf8e41174fa8b9a66e58
IvanNapsterKE <nguyoivan@gmail.com>
2022-11-08 12:55:03 +0300

    added Home.js

---
+++ my-app-frontend/src/components/Home.js
@@ -0,0 +1,36 @@
+import bg from "../img/bg.png";
+
+
+
+function Home() {
+    return (
+        <div className="container">
+
+<div className="bg">
+<img src={bg} className="bg-image bg-transparent" alt="bg" />        
+</div>
+        </div>
+
+        
+/* <div>
+
+
+
+<figure>
+    <blockquote class="blockquote">
+        <p>The world is a dangerous place to live; not because of the people who are evil, but because of the people who don't do anything about it.</p>
+    </blockquote>
+    <figcaption class="blockquote-footer">by <cite>Albert Einstein</cite></figcaption>
+</figure>
+
+
+<p class="h2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus id 
+            praesentium similique ducimus, ab atque laudantium maiores ad enim doloremque 
+            impedit delectus nemo reprehenderit voluptatibus magnam eligendi perferendis hic?</p>
+       
+
+<button type="button" class="btn btn-danger">Book Appoinment</button>
+
+</div> */
+    )}
+    export default Home;
