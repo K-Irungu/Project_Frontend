15f2cbfe1aaace5539db02a662ed3b019013c7b4
IvanNapsterKE <nguyoivan@gmail.com>
2022-11-09 14:46:38 +0300

    Added button and fixed Homepage

--- my-app-frontend/src/components/Home.js
+++ my-app-frontend/src/components/Home.js
@@ -4,33 +4,25 @@ import bg from "../img/bg.png";
 
 function Home() {
     return (
-        <div className="container">
-
-<div className="bg">
-<img src={bg} className="bg-image bg-transparent" alt="bg" />        
+      
+<div class="half-half-image-text bg-light">
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-70">
+      <div class="content row ">
+      <h1>Book a specialist</h1>
+      <p>Tabibu is an simple to use appointment booking platform that can be used by patients, doctors or hospitals to schedule hospital visits.</p>
+        <div><button class="btn btn-danger" onclick="myFunction()" id="myBtn">Book Now</button></div>
+      </div>
+    </div>
+    <div class="col-md-4 col-md-30 img-div" >
+     
+      <img class="img" src={bg} alt="bg" />
+    
+    </div>
+  </div>
 </div>
-        </div>
+</div>
 
-        
-/* <div>
-
-
-
-<figure>
-    <blockquote class="blockquote">
-        <p>The world is a dangerous place to live; not because of the people who are evil, but because of the people who don't do anything about it.</p>
-    </blockquote>
-    <figcaption class="blockquote-footer">by <cite>Albert Einstein</cite></figcaption>
-</figure>
-
-
-<p class="h2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias temporibus id 
-            praesentium similique ducimus, ab atque laudantium maiores ad enim doloremque 
-            impedit delectus nemo reprehenderit voluptatibus magnam eligendi perferendis hic?</p>
-       
-
-<button type="button" class="btn btn-danger">Book Appoinment</button>
-
-</div> */
     )}
     export default Home;
