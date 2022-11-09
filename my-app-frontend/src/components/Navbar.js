ef06ab69a8c7893f889b9be7cc53b340443d9583
IvanNapsterKE <nguyoivan@gmail.com>
2022-11-09 14:51:57 +0300

    Done with Header, Home, Nav, Footer UI

--- my-app-frontend/src/components/Navbar.js
+++ my-app-frontend/src/components/Navbar.js
@@ -4,14 +4,9 @@ import React from "react";
 function Navbar() {
     return (
         // <h1>navbar</h1>
-<div class="container-fluid">
+<div >
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger static-top">
   <div class="container">
-    {/* <a class="navbar-brand" href="#">
-      <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36" /> </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button> */}
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
@@ -20,19 +15,7 @@ function Navbar() {
         <li class="nav-item">
           <a class="nav-link" href="#">Book</a>
         </li>
-        {/* <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li>
-              <hr class="dropdown-divider" />
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li> */}
+      
       </ul>
     </div>
   </div>
